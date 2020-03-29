1604218bd2723e2b28594d899b4901ce0f55b636
“Jesse <jessehenley4@gmail.com>
2020-03-29 17:03:15 -0600

    Initialize repository

---
+++ confirm.js
@@ -0,0 +1,6 @@
+var likesCats = confirm("Do you like cats?")
+if (likesCats) {
+  console.log("You're a cool cat!")
+} else {
+  console.log("yeah, that's fine you're still cool.")
+}
---
+++ practice.js
@@ -0,0 +1,7 @@
+var fifthLetter = function (name) {
+  if (name.length < 5) {
+    return
+  }
+
+  return "The fifth letter of your name is " + name[4] + "."
+}
